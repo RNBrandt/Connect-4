bab883be3f5f8eb4969021fe4924253090d58614
Reuben Brandt & Jonathan Huang <pair+RNBrandt.jonwhuang@devbootcamp.com>
2016-01-28 14:43:26 -0800

    Add pieces to the board

--- js/application.js
+++ js/application.js
@@ -15,11 +15,36 @@ $(document).ready(function(){
     // console.log($('#' + arrayOfColumnNames[i]));
     var thisColumn = $('#' + arrayOfColumnNames[i]);
 
-    for (var j=0; j<6; j++) {
+    for (var j=5; j>=0; j--) {
       $('#' + arrayOfColumnNames[i]).append("<div class='row-div' id=" + arrayOfColumnNames[i] + "-" + j + "></div>");
     }
   }
 
+  var board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ];
 
+  var turns = 0;
+
+  $('.column-div').on('click', function(){
+    //gives us column that was clicked on
+    var column = parseInt($(this).attr('id').slice(-1));
+    //alternates red and black pieces depending on turn count
+    if (board[column].length < 6){
+      turns += 1;
+      var color = (turns % 2) ? "red" : "black";
+      //adds piece to board
+      board[column].push(color);
+      var row = board[column].length - 1;
+      $('#c' + column + '-' + row).css('background-color', color);
+    }
+    console.log(board);
+  });
 
 });
