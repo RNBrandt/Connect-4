8ba00cfd6d337e9b79db070c7863a69dd368e4f7
Bernice Anne W. Chua & Regina Wong <pair+bernicechua.iregina@devbootcamp.com>
2016-01-28 18:27:05 -0800

    Undo refactor code to generate board.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
+  var arrayOfColumnNames = [
+    "c0",
+    "c1",
+    "c2",
+    "c3",
+    "c4",
+    "c5",
+    "c6",
+  ];
 
   for (var i=0; i<7; i++) {
-    $("#board").append("<div class='column-div' id='c" + i + "'></div>");
+    $("#board").append("<div class='column-div' id=" + arrayOfColumnNames[i] + "></div>");
 
-    var thisColumn = '#c' + i;
+    // console.log($('#' + arrayOfColumnNames[i]));
+    var thisColumn = $('#' + arrayOfColumnNames[i]);
 
     for (var j=5; j>=0; j--) {
-      $('#c' + i).append("<div class='row-div' id=" + thisColumn + "-" + j + "></div>");
+      $('#' + arrayOfColumnNames[i]).append("<div class='row-div' id=" + arrayOfColumnNames[i] + "-" + j + "></div>");
     }
   }
 
