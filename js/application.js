7bfea9ae23e858fa1479566462698852f07cad53
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-04 17:48:42 -0700

    Add pictures and change text to make the game election based"

--- js/application.js
+++ js/application.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     if(turns % 2 === 0) {
       $('.column-div').hover(function() {
         var column = parseInt($(this).attr('id').slice(-1));
-        $('#c' + column + '-6').html('<img src="https://scontent-lga3-1.xx.fbcdn.net/hprofile-xpt1/v/t1.0-1/p160x160/11855885_10100926718367775_5383542053096323050_n.jpg?oh=999ca0e8dedca9ddb002bc335b8f141c&oe=573FE88A" class="token"/>')
+        $('#c' + column + '-6').html('<img src="http://pixel.nymag.com/imgs/daily/intelligencer/2015/05/06/06-hillary-clinton-immigration-giddy.w529.h529.jpg" class="token"/>')
       }, function() {
         var column = parseInt($(this).attr('id').slice(-1));
         $('#c' + column + '-6').empty();
@@ -42,7 +42,7 @@ $(document).ready(function(){
     } else {
       $('.column-div').hover(function() {
         var column = parseInt($(this).attr('id').slice(-1));
-        $('#c' + column + '-6').html('<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ1mB6fWO499QeabseMxpa67okKCEI6-0joJSRhBC-uDa98cn1ctg" class="token"/>')
+        $('#c' + column + '-6').html('<img src="http://i.kinja-img.com/gawker-media/image/upload/s--mYdC9UZC--/omhcpldxgqpwed6n1mqr.jpg" class="token"/>')
       }, function() {
         var column = parseInt($(this).attr('id').slice(-1));
         $('#c' + column + '-6').empty();
@@ -76,10 +76,10 @@ $(document).ready(function(){
 
         if (color === 'red'){
           console.log($('#c' + column + '-' + row + ' .holes'));
-          $('#c' + column + '-' + row + ' .holes').replaceWith('<img src="https://scontent-lga3-1.xx.fbcdn.net/hprofile-xpt1/v/t1.0-1/p160x160/11855885_10100926718367775_5383542053096323050_n.jpg?oh=999ca0e8dedca9ddb002bc335b8f141c&oe=573FE88A" class="circle-img centered"/>');
+          $('#c' + column + '-' + row + ' .holes').replaceWith('<img src="http://pixel.nymag.com/imgs/daily/intelligencer/2015/05/06/06-hillary-clinton-immigration-giddy.w529.h529.jpg" class="circle-img centered"/>');
           $('#c' + column + '-6').empty();
         } else {
-          $('#c' + column + '-' + row + ' .holes').replaceWith('<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ1mB6fWO499QeabseMxpa67okKCEI6-0joJSRhBC-uDa98cn1ctg" class="circle-img centered"/>');
+          $('#c' + column + '-' + row + ' .holes').replaceWith('<img src="http://i.kinja-img.com/gawker-media/image/upload/s--mYdC9UZC--/omhcpldxgqpwed6n1mqr.jpg" class="circle-img centered"/>');
           $('#c' + column + '-' + row + ' .holes').replaceWith("<div class='circle-img black-token centered'></div>");
           $('#c' + column + '-6').empty();
         }
@@ -175,8 +175,8 @@ $(document).ready(function(){
 
   var checkRed = function(string) {
     if(string.match('red,red,red,red')){
-      game_finished = "YOU'VE BEEN HUNTED!!!!!!";
-      jQuery('<div class="overlay-words overlay">' + game_finished + '<img src="https://scontent-lga3-1.xx.fbcdn.net/hprofile-xpt1/v/t1.0-1/p160x160/11855885_10100926718367775_5383542053096323050_n.jpg?oh=999ca0e8dedca9ddb002bc335b8f141c&oe=573FE88A" class="fifty-percent-size"/></div>').appendTo(document.body);
+      game_finished = "That was a Clin-ton of fun!!";
+      jQuery('<div class="overlay-words overlay">' + game_finished + '<img src="http://pixel.nymag.com/imgs/daily/intelligencer/2015/05/06/06-hillary-clinton-immigration-giddy.w529.h529.jpg" class="fifty-percent-size"/></div>').appendTo(document.body);
       setTimeout(function() {
         $('.overlay-words.overlay').fadeOut('slow');
       }, 2000);
@@ -185,8 +185,8 @@ $(document).ready(function(){
 
   var checkBlack = function(string) {
     if(string.match('black,black,black,black')){
-      game_finished = "THE HUNTER HAS BECOME THE HUNTED!!!!!!";
-      jQuery('<div class="overlay-words overlay">' + game_finished + '<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ1mB6fWO499QeabseMxpa67okKCEI6-0joJSRhBC-uDa98cn1ctg" class="fifty-percent-size"/></div>').appendTo(document.body);
+      game_finished = "You've Been Trumped!";
+      jQuery('<div class="overlay-words overlay">' + game_finished + '<img src="http://i.kinja-img.com/gawker-media/image/upload/s--mYdC9UZC--/omhcpldxgqpwed6n1mqr.jpg" class="fifty-percent-size"/></div>').appendTo(document.body);
       setTimeout(function() {
         $('.overlay-words.overlay').fadeOut('slow');
       }, 2000);
